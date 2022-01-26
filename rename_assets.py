89f3c6c606bf3cb7c2a70a5b3d115f31f6eb5e20
zoonyeok <ryubb09@gmail.com>
2022-01-26 10:52:10 +0900

    rename_assets

--- rename_assets.py
+++ rename_assets.py
@@ -1,8 +1,37 @@
-# 내장함수
+import unreal
 
-import math
+def rename_assets(search_pattern, replace_pattern, use_case):
+    # instance of unreal classes
+    system_lib = unreal.SystemLibrary()
+    editor_util = unreal.EditorUtilityLibrary()
+    string_lib = unreal.StringLibrary()
 
-a = math.isclose(0.1 + 0.2, 0.3)
-print(a)
+    # get the selected assets
+    selected_assets = editor_util.get_selected_assets()
+    num_assets = len(selected_assets)
+    replaced = 0
 
-a = 0.1 + 0.2
+    unreal.log("Selected {} assets".format(num_assets))
+
+    # loop over each asset and rename
+    for asset in selected_assets:
+        asset_name = system_lib.get_object_name(asset)
+
+        # 에셋 이름이 대체될 텍스트를 포함하는지 체크
+        if string_lib.contains(asset_name, search_pattern, use_case=use_case):
+            search_case = unreal.SearchCase.CASE_SENSITIVE if use_case else unreal.SearchCase.IGNORE_CASE
+            replaced_name = string_lib.replace(
+                asset_name, search_pattern, replace_pattern, search_case=search_case)
+            editor_util.rename_asset(asset, replaced_name)
+
+            replaced += 1
+            unreal.log("Replaced {} with {}".format(asset_name, replaced_name))
+
+        else:
+            unreal.log(
+                "{} did not match the search pattern, was skipped".format(asset_name))
+
+    unreal.log("Replaced {} of {} assets".format(replaced, num_assets))
+
+
+rename_assets("Death", "Dead", False)
